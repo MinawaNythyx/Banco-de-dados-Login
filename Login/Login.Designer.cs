873e58fe352dc7b00f65b16818b27b3db566a797
Victor Lima <victor.lima@corporativo.octo>
2019-11-20 11:27:29 -0300

    Adicionar arquivos de projeto.

---
+++ Login/Login.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.user = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.usertxt = new System.Windows.Forms.TextBox();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.createlogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(155, 133);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(75, 23);
+            this.loginbtn.TabIndex = 0;
+            this.loginbtn.Text = "Login";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(270, 133);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(75, 23);
+            this.exitbtn.TabIndex = 1;
+            this.exitbtn.Text = "Exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Location = new System.Drawing.Point(35, 40);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(55, 13);
+            this.user.TabIndex = 2;
+            this.user.Text = "Username";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(35, 91);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(53, 13);
+            this.pass.TabIndex = 3;
+            this.pass.Text = "Password";
+            // 
+            // usertxt
+            // 
+            this.usertxt.Location = new System.Drawing.Point(117, 37);
+            this.usertxt.Name = "usertxt";
+            this.usertxt.Size = new System.Drawing.Size(228, 20);
+            this.usertxt.TabIndex = 4;
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(117, 88);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.Size = new System.Drawing.Size(228, 20);
+            this.passtxt.TabIndex = 5;
+            // 
+            // createlogin
+            // 
+            this.createlogin.Location = new System.Drawing.Point(38, 133);
+            this.createlogin.Name = "createlogin";
+            this.createlogin.Size = new System.Drawing.Size(75, 23);
+            this.createlogin.TabIndex = 6;
+            this.createlogin.Text = "Create";
+            this.createlogin.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 184);
+            this.Controls.Add(this.createlogin);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.usertxt);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.loginbtn);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Label user;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.TextBox usertxt;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.Button createlogin;
+    }
+}
+
---
+++ Login/databaseUse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    class databaseUse
+    {
+        public void connect()
+        {
+            SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\victor.lima\source\repos\Login\Login\DB\DatabaseLogin.mdf;Integrated Security=True;Connect Timeout=30");
+            //string query = "SELECT loginID"
+        }
+    }
+}
